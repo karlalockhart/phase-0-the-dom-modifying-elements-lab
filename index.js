cc35f337ac5ef2e29001fba5129ad9d84bfc1c12
Karla Lockhart <kblockhart@gmail.com>
2023-08-20 19:11:09 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code here!
+/*
+document.querySelector('main#main')
+
+newHeader.nodeName
+
+h1  
+
+newHeader.id(victory)
+
+newHeader.innerHTML
+
+"Karla is the champion"
+*/
+
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = "YOUR-NAME is the champion"
